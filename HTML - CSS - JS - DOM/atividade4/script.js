44137ef33f145aaf60f5cb97375657ce1dcfcc05
saraalmada <saraalmadaj5@gmail.com>
2025-08-07 18:17:01 -0300

    finalizei atividade de loops

--- HTML - CSS - JS - DOM/atividade4/script.js
+++ HTML - CSS - JS - DOM/atividade4/script.js
@@ -1,5 +1,7 @@
 // 1- Uma variável numeroDia guarda um número de 1 a 7 digitado pelo usuário. Crie uma estrutura if/else que exiba o nome do dia da semana correspondente (1 para "Domingo", 7 para "Sábado"). Se o número não estiver nesse intervalo, exiba "Número inválido".
+
 let numeroDia = Number(prompt('Digite um número de 1 a 7: '))
+
 if (numeroDia === 1) {
     console.log('Domingo')
 } else if (numeroDia === 2) {
@@ -16,13 +18,14 @@ if (numeroDia === 1) {
     console.log('Sábado')
 } else {
     console.log('Número inválido')
-
 }
 
 
 // 2- Duas variáveis, usuário e senha, guardam as informações de login. Verifique se o usuário é "admin". E a senha é "1234". Se as duas condições forem verdadeiras, exiba "Acesso concedido!". Caso contrário, exiba "Acesso negado!".
+
 let usuario = prompt('Usuário: ')
 let senha = prompt('Senha: ')
+
 if (usuario === 'admin' && senha === '1234') {
     console.log('Acesso concedido!')
 } else {
@@ -31,8 +34,10 @@ if (usuario === 'admin' && senha === '1234') {
 
 
 // 3- Uma pessoa quer comprar um produto. Crie uma condição que verifica se a pessoa é membroVip OU se o valorDaCompra é maior que R$1000. Se uma dessas condições for verdadeira, exiba "Compra com desconto!". Senão, exiba "Compra com preço normal.".
+
 let membroVip = false
 let valorDaCompra = 1200
+
 if (membroVip == true || valorDaCompra > 1000) {
     console.log('Compra com desconto!')
 } else {
@@ -47,7 +52,9 @@ for (let i = 1; i <= 10; i++) {
 
 
 // 5- Crie um laço for que some os números de 1 a 5. Crie uma variável soma fora do laço para guardar o resultado. A cada volta do laço, adicione o número atual à variável soma. No final, exiba o resultado.
+
 let soma = 0
+
 for (let i = 1; i <= 5; i++) {
     soma += i
 }
@@ -55,6 +62,7 @@ console.log(soma)
 
 
 // 6- Use um laço for para criar a tabuada do número 5, de 1 a 10. A saída deve ser no formato "5 x 1 = 5", "5 x 2 = 10", e assim por diante.
+
 for (let i = 1; i <= 10; i++) {
     let res = 5 * i
     console.log(`5 x ${i} = ${res}`)
@@ -62,15 +70,19 @@ for (let i = 1; i <= 10; i++) {
 
 
 // 7- Escreva um código usando o laço DO..While que faça a contagem REGRESSIVA de 10 ate 1. Dica: Assim como valor++ soma 1 no valor atual, valor-- subtrai 1 do valor atual.
+
 let contador = 10
+
 do {
     console.log(contador)
     contador--
 } while (contador >= 1)
 
 
-// 8- Exibir números pares de 1 a 20,mostre apenas os pares de 1 a 20 usando for ou while.
+// 8- Exibir números pares de 1 a 20, mostre apenas os pares de 1 a 20 usando for ou while.
+
 let i = 1
+
 while (i <= 20) {
     if (i % 2 === 0) {
         console.log(i)
@@ -80,6 +92,7 @@ while (i <= 20) {
 
 
 // 9- Crie um jogo onde um numeroSecreto é definido como 15. Use um laço while para continuar "pedindo" um palpite (simulado por uma variável palpite que você altera manualmente) até que o palpite seja igual ao número secreto. A cada tentativa errada, exiba "Você errou, tente de novo!". Quando acertar, exiba "Parabéns, você acertou!"
+
 let numeroSecreto = 15
 let palpite = null
 
@@ -94,7 +107,8 @@ while (palpite !== numeroSecreto) {
 console.log('Parabéns, você acertou!')
 
 
-// 10- Em uma promoção, você tem direito a sortear um número da sorte uma vez. Se o número sorteado for maior que 50, você pode sortear de novo. Use um laço do...while para simular isso. O sorteio (simulado por um  variável) acontece dentro do laço, e o laço continua enquanto o numeroSorteado for maior que 50.
+// 10- Em uma promoção, você tem direito a sortear um número da sorte uma vez. Se o número sorteado for maior que 50, você pode sortear de novo. Use um laço do...while para simular isso. O sorteio (simulado por uma variável) acontece dentro do laço, e o laço continua enquanto o numeroSorteado for maior que 50.
+
 let numeroSorteado = null
 
 do {
@@ -105,4 +119,4 @@ do {
     }
 } while (numeroSorteado > 50)
 
-console.log('Parabéns! Número da sorte final:', numeroSorteado)
+console.log(`FIM! O número ${numeroSorteado} é menor que 50.`)
