ca4f37a2fff4a00d9b6f970ab8e1cb7917d41965
saraalmada <saraalmadaj5@gmail.com>
2025-08-20 17:16:59 -0300

    Expand type conversion examples and output formatting
    Added new examples for user registration and purchase decision using type conversion. Improved output formatting for clarity and corrected IMC calculation print statement.

--- JAVA/Aulas_ElasTech/src/aulas_java/Aula_03_conversaoTipos.java
+++ JAVA/Aulas_ElasTech/src/aulas_java/Aula_03_conversaoTipos.java
@@ -1,7 +1,5 @@
 package aulas_java;
 
-java.util.*;
-
 public class Aula_03_conversaoTipos {
 
 	public static void main(String[] args) {
@@ -11,8 +9,8 @@ public class Aula_03_conversaoTipos {
 		int valor3 = Integer.parseInt(valor1);
 		int valor4 = Integer.parseInt(valor2);
 		
-		System.out.println(valor3);
-		System.out.println(valor4);
+		System.out.println("Primeiro valor: " + valor3);
+		System.out.println("Segundo valor: " + valor4);
 		
 		// Calculadora de IMC
 		String peso = "78.5";
@@ -22,16 +20,38 @@ public class Aula_03_conversaoTipos {
 		double alturaC = Double.parseDouble(altura);
 		
 		double imc = pesoC / (alturaC * alturaC);
-		System.out.printf("Seu IMC é: %.2f" + imc);
+		System.out.printf("Seu IMC é: %.2f%n", imc);
+		
 		
 		// Cadastro Simples
+		String nome = "Sara";
+		String idade = "26";
+		String salario = "3200.50";
 		
+		int minhaIdade = Integer.parseInt(idade);
+		double salarioImaginario = Double.parseDouble(salario);
+		String apresentacao = String.format("O usuário %s, de %d anos, tem um salário de R$ %.2f", nome, minhaIdade, salarioImaginario);
+		
+		System.out.println(apresentacao);
 		
 		// O Erro Proposital
-		
+//		String numeroInvalido = "123z";
+//		
+//		int numeroInv = Integer.parseInt(numeroInvalido);
+//		//Exception in thread "main" java.lang.NumberFormatException: For input string: "123z"
 		
 		// Decisão de Compra
+		String saldoConta = "500.75";
+		String precoProduto = "499.99";
+		String temCredito = "true";
 		
+		double saldo = Double.parseDouble(saldoConta);
+		double preco = Double.parseDouble(precoProduto);
+		boolean credito = Boolean.parseBoolean(temCredito);
+		
+		System.out.println("Saldo na conta: R$" + saldo);
+		System.out.println("Preço do produto: R$" + preco);
+		System.out.println("Tem crédito? " + credito);
 	
 	}
 
